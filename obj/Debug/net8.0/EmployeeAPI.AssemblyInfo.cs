2ecfd59096aea5db1aaeb201f65816e7f5e4ce4a
Ashish <ashishdharme932@gmail.com>
2025-03-19 16:53:44 +0530

    Initial commit

--- obj/Debug/net8.0/EmployeeAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/EmployeeAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90dbbcf42bf79d2c5ee4d119cb0b6c27f9740e5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
